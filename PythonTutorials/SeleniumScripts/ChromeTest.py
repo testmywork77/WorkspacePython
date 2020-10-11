9956f261a8d33e84d6b811746b54851b2abe07cf
testmywork77 <testmywork77@gmail.com>
2020-10-11 19:32:31 +0100

    Initial SeleniumPython Project SetUp

--- PythonTutorials/SeleniumScripts/ChromeTest.py
+++ PythonTutorials/SeleniumScripts/ChromeTest.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-
+# To supress this warning: DeprecationWarning: executable_path has been deprecated,
+# please pass in a Service object driver = webdriver.Edge(executable_path="C:/WebDrivers/msedgedriver.exe")
 # C:\\WebDrivers\\chromedriver.exe -Backward Slash, need escape character
 # C:/WebDrivers/chromedriver.exe -Forward Slash, no need escape character
-driver = webdriver.Chrome(executable_path="C:/WebDrivers/chromedriver.exe")
+
+objService = Service("C:/WebDrivers/chromedriver.exe")
+driver = webdriver.Chrome(service=objService)
 print(type(driver))
 driver.get("http://www.google.com")
 myPageTitle = driver.title
---
+++ SeleniumPython/Scripts_NVL/Basics1.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome(executable_path="C:/WebDrivers/chromedriver.exe")
+driver.implicitly_wait(5)
+driver.get("http://www.google.com")
+print(driver.title)
+
+driver.find_element(By.CSS_SELECTOR, 'div#introAgreeButton').click()
+driver.find_element(By.NAME, 'q').send_keys("naveen automationlabs")
+
+optionList = driver.find_element(By.CSS_SELECTOR, 'ul.erkvQe li span')
+print(len(optionList))
+
+for option in optionList:
+    print(option)
+
+time.sleep(5)
+driver.quit()
+
