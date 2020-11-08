7d667e111f12842063d47019c830d36c692ec18b
testmywork77 <testmywork77@gmail.com>
2020-11-08 20:05:54 +0000

    SeleniumV4 Scripts added_1

--- SeleniumV4Python/SeleniumScripts/ByBrowser/EdgeTest.py
+++ SeleniumV4Python/SeleniumScripts/ByBrowser/EdgeTest.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service
 
 
+# In Selenium 4, executable_path has been deprecated,
 # C:\\WebDrivers\\msedgedriver.exe -Backward Slash, need escape character
 # C:/WebDrivers/msedgedriver.exe -Forward Slash, no need escape character
-driver = webdriver.Edge(executable_path="C:/WebDrivers/msedgedriver.exe")
+objService = Service("C:/WebDrivers/msedgedriver.exe")
+driver = webdriver.Edge(service=objService)
 print(type(driver))
 driver.get("http://www.google.com")
 myPageTitle = driver.title
