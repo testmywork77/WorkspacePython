c4e240544a709096c41672c39fbca8c240ee55e5
testmywork77 <testmywork77@gmail.com>
2020-12-30 19:35:25 +0000

    Added functions in SQLite3 repository folder

--- python-pocs/sqlite3_examples/app.py
+++ python-pocs/sqlite3_examples/app.py
@@ -6,12 +6,15 @@ import repository
 # Delete customer from Customers table
 # repository.delete_customer('5')
 
-
 # Add customers to Customers table
-customerList = [
-    ('fname98', 'lname98', 'name98@gmail.com'),
-    ('fname99', 'lname99', 'name99@gmail.com')    
-]
-repository.add_customers(customerList)
+# customerList = [
+#     ('fname98', 'lname98', 'name98@gmail.com'),
+#     ('fname99', 'lname99', 'name99@gmail.com')    
+# ]
+# repository.add_customers(customerList)
+
+# Email LookUp
+repository.email_lookup('name98@gmail.com')
+
 # Show all the customers
-repository.get_customer_all()
+# repository.get_customer_all()
--- python-pocs/sqlite3_examples/repository.py
+++ python-pocs/sqlite3_examples/repository.py
@@ -34,7 +34,6 @@ def add_customer(first_name, last_name, email):
     # Close connection
     conn.close()
 
-
 def add_customers(customerList):
     # Connect to database
     conn = sqlite3.connect('customer.db')
@@ -51,7 +50,7 @@ def add_customers(customerList):
     # Close connection
     conn.close()
 
-def delete_customer(id):
+def delete_customer(rowid):
     # Connect to database
     conn = sqlite3.connect('customer.db')
 
@@ -59,10 +58,52 @@ def delete_customer(id):
     c = conn.cursor()
    
     # Insert a record
-    c.execute("DELETE FROM Customers WHERE rowid = (?)", id)
+    c.execute("DELETE FROM Customers WHERE rowid = (?)", rowid)
     # Commit command
     conn.commit()
 
     # Close connection
     conn.close()
 
+def update_customer(rowid):
+    # Connect to database
+    conn = sqlite3.connect('customer.db')
+
+    # Create a cursor
+    c = conn.cursor()
+    
+    # Query the customer before update
+    c.execute(f"SELECT rowid, * FROM Customers WHERE rowid = {rowid}")
+    # Fetch record
+    print(c.fetchone())
+
+    # Update last_name for #1st reord
+    c.execute(f"Update customers SET last_name = 'lname1' WHERE rowid = {rowid}")
+
+    # Commit command
+    conn.commit()
+
+    # Query the customer after update
+    c.execute(f"SELECT rowid, * FROM Customers WHERE rowid = {rowid}")
+    # Fetch record
+    print(c.fetchone())
+
+    print('Commands executed successfuly')
+
+    # Close connection
+    conn.close()
+
+def email_lookup(email):
+    # Connect to database
+    conn = sqlite3.connect('customer.db')
+
+    # Create a cursor
+    c = conn.cursor()
+    
+    # Query the customer before update
+    c.execute("SELECT rowid, * FROM Customers WHERE email = (?)", (email,))
+    # Fetch record
+    print(c.fetchone())
+
+    # Close connection
+    conn.close()
