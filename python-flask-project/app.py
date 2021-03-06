c9c8d3090c1539c97990daa99c4cef39ad79b043
testmywork77 <testmywork77@gmail.com>
2021-03-06 22:59:25 +0000

    Flask-RESTful package added

--- python-flask-project/app.py
+++ python-flask-project/app.py
@@ -1,7 +1,7 @@
 # flask is module/package, Flask is Class, jsonify is method
 # camelCase -module(i.e. file name), package, method
 # Pascal - Class
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -16,6 +16,11 @@ stores = [
         ]
     }
 ]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 # POST -used to receive data (from Server point of view)
@@ -35,7 +40,13 @@ def create_store():
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    # Iterate over stores
+    # If the store name matches, return it
+    # If non match, return an error message
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'Store not found'})
 
 
 # GET /store
@@ -47,13 +58,25 @@ def get_stores():
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'Store not found'})
 
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'Store not found'})
 
 
 # if __name__ == '__main__':
