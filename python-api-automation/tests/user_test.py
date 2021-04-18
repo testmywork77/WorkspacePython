25878998e4c0249e90afdb56cfdbf44997229cee
Venkata Mannepalli <testmywork77@gmail.com>
2021-04-18 11:41:37 +0100

    User delete functionality done

--- python-api-automation/tests/user_test.py
+++ python-api-automation/tests/user_test.py
@@ -1,69 +1,103 @@
 import requests
 from assertpy.assertpy import assert_that
-from json import dumps
+from json import dumps, loads
 from config import BASE_URI
 
 
-def test_read_all_has_firstname1():
-    users, response = get_all_users()
-    assert_that(response.status_code).is_equal_to(requests.codes.ok)
-    assert_that(response.status_code).is_equal_to(200)
+def test_user_get_all():
+    users = get_all_users()
+    assert_that(users).is_not_empty()
+
+
+def test_user_get_by_id():
+    user_id = 1
+    # Step 1: Call "get_user_by_id", return user object
+    user = get_user_by_id(user_id)
+    # Step 2: Check weather user object is not null or not
+    assert_that(user).is_not_empty()
+    assert_that(user["id"]).is_equal_to(user_id)
+
+
+def test_user_get_based_on_firstname():
+    users = get_all_users()
     first_names = [user['firstName'] for user in users]  # Python - List Comprehension
     assert_that(first_names).contains('firstName1')
 
 
 def test_user_can_be_added():
-    first_name = create_new_unique_user()  # First, create user
-    users, _ = get_all_users()  # Second , Get all users
-    new_user = search_users_by_first_name(first_name, users)[0]  # Third, check weather the new user exist in users list
-    assert_that(new_user).is_not_empty()
-    # Delete the created user
-    user_id_to_be_deleted = new_user['id']
-    url = f"{BASE_URI}/{user_id_to_be_deleted}"
-    # Step 6: Trigger the delete request
-    response = requests.delete(url)
-    assert_that(response.status_code).is_equal_to(200)
+    user_id = 99
+
+    # Step 1: Create user dictionary
+    user_dict = user_dict_create(user_id)
+
+    # Step 2: Call "create_new_user" method with post request
+    user_new = create_new_user(user_dict)  # First, create user
+    assert_that(user_new).is_not_empty()
+
+    # Step 3: Call "get_user_by_id", return user object
+    user = get_user_by_id(user_new["id"])
+    assert_that(user).is_not_empty()
+
+    # Step 4: Call "
+
+
+    # # Third, check weather the new user exist in users list
+    # new_user = search_users_by_first_name(first_name, users)[0]
+    # assert_that(new_user).is_not_empty()
+    # # Delete the created user
+    # user_id_to_be_deleted = new_user['id']
+    # url = f"{BASE_URI}/{user_id_to_be_deleted}"
+    # # Step 6: Trigger the delete request
+    # response = requests.delete(url)
+    # assert_that(response.status_code).is_equal_to(200)
 
 
 def test_user_can_be_deleted():
-    # Step 1: Create new user and return firstName
-    new_user_first_name = create_new_unique_user()
-    # Step 2: Get all users
-    users, _ = get_all_users()
-    # Step 3: Search weather the new_user exists in all users based on firstName
-    # print(type(search_users_by_first_name(new_user_first_name, users)))
-    # Step 4: Get the new user object
-    new_user = search_users_by_first_name(new_user_first_name, users)[0]
-    # print(new_user)
-    # print(type(new_user))
-    # Step 5: Get id of user object
-    user_id_to_be_deleted = new_user['id']
-    url = f"{BASE_URI}/{user_id_to_be_deleted}"
-    # Step 6: Trigger the delete request
-    response = requests.delete(url)
-    assert_that(response.status_code).is_equal_to(200)
+    # Arrange - Create user
+    user_id = 99
+    user_dict = user_dict_create(user_id)   # user dictionary
+    user_new = create_new_user(user_dict)   # return created new user
+    assert_that(user_new).is_not_empty()  # check weather user is empty/null
 
+    # Act - Delete user
+    delete_user_by_id(user_new["id"])
 
+    # Assert - Check user exists or not
+    user = get_user_by_id(user_id)  # return user dictionary
+
+        
 # List Comprehension returns list
 def search_users_by_first_name(first_name, users):
     return [user for user in users if user['firstName'] == first_name]
 
 
+# Returns user dictionary
+def get_user_by_id(user_id):
+    url = f"{BASE_URI}/{user_id}"
+    response = requests.get(url)
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.status_code).is_equal_to(requests.codes.ok)
+    user = response.json()
+    assert_that(user["id"]).is_equal_to(user_id)
+    return user
+
+
+# Returns user dictionary
+def delete_user_by_id(user_id):
+    url = f"{BASE_URI}/{user_id}"
+    response = requests.delete(url)
+    assert_that(response.status_code).is_equal_to(200)
+
+
+# Returns users i.e. List of user dictionaries
 def get_all_users():
     response = requests.get(BASE_URI)
+    assert_that(response.status_code).is_equal_to(200)
     users = response.json()
-    return users, response
+    return users
 
 
-def create_new_unique_user():
-    new_user_id = 2
-    user_dict = {
-        "id": new_user_id,
-        "firstName": f"firstName{new_user_id}",
-        "lastName": f"lastName{new_user_id}",
-        "loc": f"location{new_user_id}",
-        "timestamp": ""
-    }
+def create_new_user(user_dict):
     # dumps method converts "python dictionary" to "json". It's Serialization
     payload = dumps(user_dict)
     headers = {
@@ -72,4 +106,17 @@ def create_new_unique_user():
     }
     response = requests.post(url=BASE_URI, data=payload, headers=headers)
     assert_that(response.status_code).is_equal_to(201)
-    return user_dict["firstName"]
+    user_created = loads(response.text)
+    return user_created
+
+
+def user_dict_create(user_id):
+    new_user_id = user_id
+    user_dict = {
+        "id": new_user_id,
+        "firstName": f"firstName{new_user_id}",
+        "lastName": f"lastName{new_user_id}",
+        "loc": f"location{new_user_id}",
+        "timestamp": ""
+    }
+    return user_dict
