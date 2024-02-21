4b128d3f42bb096dc659b2aa1bc4c38dd6b8c6e9
testmywork <testmywork77@gmail.com>
2024-02-21 18:42:47 +0000

    python-core-programming project added

---
+++ python-core-programming/fundamentals/‌‌nonPrimitiveDataType.py
@@ -0,0 +1,11 @@
+# List Example 
+fruits = ["apple", "banana", "cherry"] 
+print("List Example:", fruits)
+ 
+# Tuple Example
+coordinates = (3, 7)
+print("Tuple Example:", coordinates)
+ 
+ # Dictionary Example 
+person = {"name": "Alice", "age": 25, "is_student": True}
+print("Dictionary Example:", person)
